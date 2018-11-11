7530c1e74d851a793703052763ae3dab15a547b9
Abhishek Nigam <abhisheknigam1996@gmail.com>
2018-11-11 22:03:00 +0530

    Added basic animations

--- src/App.js
+++ src/App.js
@@ -4,21 +4,29 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import NotificationPanel from './components/NotificationPanel/NotificationPanel';
 
-const baseUrl = 'http://localhost:3001';
+const baseUrl = 'https://notifui.now.sh';
 
 class App extends Component {
+
   state = {
     isNotificationOpen: false,
     noOfUnreadNotifications: 0,
     notifications: [],
   }
 
+  ////////////////////
   // Lifecycle Methods
+  ////////////////////
+
   componentDidMount = () => {
     this.getNotifications();
   }
 
-  // Handlers
+  ///////////////////
+  // Handlers Methods
+  ///////////////////
+
+
   toggleIsNotificationOpen = () => {
 
     this.setState(prevState => {
@@ -52,37 +60,6 @@ class App extends Component {
     this.getNotifications();
   }
 
-  // Helper Methods
-  getNotifications = () => {
-    fetch(`${baseUrl}/getNotifications`, {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      method: 'GET'
-    })
-      .then(res => res.json())
-      .then(res => {
-
-        console.log(res);
-
-        let notifications = res.data;
-        let unread = 0;
-
-        for (let i = 0; i < notifications.length; i++) {
-          if (notifications[i].unread === true)
-            unread += 1;
-        }
-
-        this.setState({
-          noOfUnreadNotifications: unread,
-          notifications: notifications,
-          isNotificationOpen: false
-        });
-
-      });
-  };
-
   dismissAllHandler = () => {
     this.setState(prevState => {
       this.dismissNotifications(prevState.notifications);
@@ -107,42 +84,69 @@ class App extends Component {
     });
   }
 
+
+  ///////////////////
+  // Helper Methods
+  ///////////////////
+
+  getNotifications = () => {
+    fetch(`${baseUrl}/getNotifications`, {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'GET'
+    })
+      .then(res => res.json())
+      .then(res => {
+        let notifications = res.data;
+        let unread = 0;
+
+        for (let i = 0; i < notifications.length; i++) {
+          if (notifications[i].unread === true)
+            unread += 1;
+        }
+
+        this.setState({
+          noOfUnreadNotifications: unread,
+          notifications: notifications,
+          isNotificationOpen: false
+        });
+      });
+  };
+
   readNotifications = (notifications) => {
     let notification_ids = notifications.map(el => el._id);
+
     fetch(`${baseUrl}/readNotifications`, {
       headers: {
-        'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
       method: "POST",
       body: JSON.stringify({ notifications: notification_ids })
     })
       .then(res => {
-        console.log(res);
-        console.log(`Read notifications ${notification_ids}`)
+        console.log(`Result: Success, Info: Read notifications ${notification_ids}`)
+      })
+      .catch(err => {
+        console.log(`Result: Error, Info: ${err}`);
       });
   };
 
   dismissNotifications = (notifications, isOne = false) => {
-    // console.log('received in function', notifications);
-
     let notification_ids = isOne ? [notifications._id] : notifications.map(el => el._id);
-
-
-    let obj_to_send = { notifications: notification_ids };
-    console.log('sending object', obj_to_send);
 
     fetch(`${baseUrl}/dismissNotifications`, {
       headers: {
-        'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
       method: "POST",
-      body: JSON.stringify(obj_to_send)
+      body: JSON.stringify({ notifications: notification_ids })
     })
       .then(res => {
-        console.log(res);
-        console.log(`Dismissed notifications ${notification_ids}`)
+        console.log(`Result: Success, Info: Dismissed notifications ${notification_ids}`)
+      })
+      .catch(err => {
+        console.log(`Result: Error, Info: ${err}`);
       });
   }
 
--- src/components/NotificationPanel/NotificationPanel.js
+++ src/components/NotificationPanel/NotificationPanel.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
 import './NotificationPanel.css'
 import NotificationEntry from '../NotificationEntry/NotificationEntry';
 
@@ -6,33 +8,39 @@ import NotificationEntry from '../NotificationEntry/NotificationEntry';
 const notificationPanel = props => {
 
     const notifications = props.notifications.length > 0 ? (
-        <div className="notification-scroll-container">
-            {props.notifications.map(notification => <NotificationEntry
-                key={notification._id}
-                author_image={notification.author_image}
-                author_name={notification.author_name}
-                author_action={notification.author_action}
-                dismiss={() => { props.dismissOne(notification) }} />)}
-        </div>
+        <TransitionGroup className="notification-scroll-container">
+            {props.notifications.map(notification =>
+                <CSSTransition key={notification._id} timeout={300} classNames="notification">
+                    <NotificationEntry
+
+                        author_image={notification.author_image}
+                        author_name={notification.author_name}
+                        author_action={notification.author_action}
+                        dismiss={() => { props.dismissOne(notification) }} />
+                </CSSTransition>
+            )}
+        </TransitionGroup>
     ) : (
             <div className="notification-message">
                 <span>All caught up!</span>
             </div>
         );
 
-    return props.isOpen ? (
-        <div className="notification-panel">
-            <div className="notification-panel-header">
-                <span>Notifications</span>
-                {props.unread > 0 ? <span className="badge-count-panel">{props.unread}</span> : null}
-                <span className="dismiss-all" onClick={props.dismissAll}><i className="fas fa-circle"></i></span>
-                <span className="caret-up"><i className="fas fa-caret-up"></i></span>
+    return (
+        <CSSTransition in={props.isOpen} unmountOnExit timeout={500} classNames="panel">
+            <div className="notification-panel">
+                <div className="notification-panel-header">
+                    <span>Notifications</span>
+                    {props.unread > 0 ? <span className="badge-count-panel">{props.unread}</span> : null}
+                    <span className="dismiss-all" onClick={props.dismissAll}><i className="fas fa-circle"></i></span>
+                    <span className="caret-up"><i className="fas fa-caret-up"></i></span>
+                </div>
+                <div className="notification-panel-content">
+                    {notifications}
+                </div>
             </div>
-            <div className="notification-panel-content">
-                {notifications}
-            </div>
-        </div>
-    ) : null;
+        </CSSTransition>
+    );
 }
 
 export default notificationPanel;
